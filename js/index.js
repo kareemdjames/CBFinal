5a2b712abd8692d39dbb4cec138ca1f99cbdd128
Kareem James <kareemdjames@users.noreply.github.com>
2017-09-10 23:36:38 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+  // slideshow function
+  var slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("miSlides");
+    
+    if(n > x.length) {
+      slideIndex = 1;
+    }
+    if(n < 1) {
+      slideIndex = x.length;
+    }
+    for(i = 0; i < x.length; i += 1) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }
+  //tabbed list function
+  function openMe(inside) {
+    var i, content;
+    content = document.getElementsByClassName("content");
+      for(i = 0; i < content.length; i +=1) {
+        content[i].style.display = "none"
+      }
+      document.getElementById(inside).style.display = "block";
+  }
+  //google maps function
+  function btmMap() {
+    var mapProp = { 
+      center: new google.maps.LatLng(40.890141,-73.855152),
+      zoom: 17,
+    };
+    var map = new googe.maps.Map(document.getElementById("map"),mapProp);
+  }
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHKysy67fniqtOPr5vyXrOVaWuvUTZZmU&callback=myMap">
+
+
+
+
+
+
